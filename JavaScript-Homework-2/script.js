b1cb6e48325ead64d1e7336ecd0efd96780ebf21
MartinMitikj <martin.mitic69@yahoo.com>
2023-12-07 08:13:35 -0800

    Homework

---
+++ JavaScript-Homework-2/script.js
@@ -0,0 +1,44 @@
+console.log("Hello")
+
+let year = parseInt (prompt("Enter year"));
+let formula = (year - 4) % 12;
+
+if (formula == 0){
+    alert("Rat")
+}
+else if (formula == 1){
+    alert("Ox")
+}
+else if (formula == 2){
+    alert("Tiger")
+}
+else if (formula == 3){
+    alert("Rabbit")
+}
+else if (formula == 4){
+    alert("Dragon")
+}
+else if (formula == 5){
+    alert("Snake")
+}
+else if (formula == 6){
+    alert("Horse")
+}
+else if (formula == 7){
+    alert("Goat")
+}
+else if (formula == 8){
+    alert("Monkey")
+}
+else if (formula == 9){
+    alert("Rooster")
+}
+else if (formula == 10){
+    alert("Dog")
+}
+else if (formula == 11){
+    alert("Pig")
+}
+else {
+    alert("Pig")
+}
---
+++ JavaScript-Homewrok-1/script.js
@@ -0,0 +1,11 @@
+console.log("Hello")
+
+let numberOfPhones = 30;
+let price = 119.95;
+let taxRate = 0.05;
+
+let priceOfAllPhones = numberOfPhones * price; 
+let tax = priceOfAllPhones * taxRate
+let result = priceOfAllPhones + tax;
+
+console.log(result);
